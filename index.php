352de8e167e81f5efc292d002cfa569aefb1e70c
zhuyan <yan.zhu01@billionsfinance.cn>
2016-07-14 18:15:01 +0800

    初始版本！

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * this is php construct is only for tianmingmng
+ * you can visit www.tianmingming.com for more information
+ * date: 2016-07-14
+ */
+define('app_path','./app');
+require __DIR__.'/bootstrap/bootstrap.php';
+$app = new App();
+$app->start();
