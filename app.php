fc026a5d85a8a2890558e3870d555adf58bac4a7
zhuyan <yan.zhu01@billionsfinance.cn>
2016-10-14 17:29:28 +0800

    报告错误！

--- app.php
+++ app.php
@@ -4,7 +4,7 @@
  * you can visit www.tianmingming.com for more information
  * date: 2016-07-14
  */
-error_reporting(E_ALL);
+error_reporting(E_ERROR);
 ini_set('display_errors', '1');
 define('app_path','./app');
 define('index_path',__DIR__);
