c03a3e68d04bab0d59af9f7b1261cbc3956e6bd7
zhuyan <yan.zhu01@billionsfinance.cn>
2016-11-14 15:38:36 +0800

    地图随心变！

--- framework/mail.php
+++ framework/mail.php
@@ -34,8 +34,8 @@ class Mail{
 //        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
         if (!$mail->send()) {
-//            echo 'Message could not be sent.';
-//            echo 'Mailer Error: ' . $mail->ErrorInfo;
+            echo 'Message could not be sent.';
+            echo 'Mailer Error: ' . $mail->ErrorInfo;
         } else {
 //            echo 'Message has been sent';
         }
